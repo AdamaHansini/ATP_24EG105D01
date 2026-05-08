a14a2cb2bdc5c011a1f1db971bda4049a753634c
AdamaHansini <adamahansini@gmail.com>
2026-05-08 10:15:15 +0530

    Refactor server.js for environment variable support

--- WEEK-6/BackEnd/server.js
+++ WEEK-6/BackEnd/server.js
@@ -2,25 +2,37 @@ import exp from "express";
 import { connect } from "mongoose";
 import { empRoute } from "./API/empApp.js";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = exp();
-//add cors middleware
+
+// CORS middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: "*",
   }),
 );
-//body parser middleware
+
+// body parser middleware
 app.use(exp.json());
-//emp api middleware
+
+// emp api middleware
 app.use("/emp-api", empRoute);
 
-//DB connection
+// DB connection
 const connectDB = async () => {
   try {
-    await connect("mongodb://localhost:27017/empdb");
+    await connect(process.env.MONGO_URI);
+
     console.log("DB connected");
-    app.listen(4000, () => console.log("server listening on port 4000.."));
+
+    const PORT = process.env.PORT || 4000;
+
+    app.listen(PORT, () =>
+      console.log(`server listening on port ${PORT}..`)
+    );
   } catch (err) {
     console.log("err in DB connection", err.message);
   }
@@ -28,7 +40,7 @@ const connectDB = async () => {
 
 connectDB();
 
-//error handling middleware
+// error handling middleware
 app.use((err, req, res, next) => {
   console.log("err in middleware:", err.message);
 
